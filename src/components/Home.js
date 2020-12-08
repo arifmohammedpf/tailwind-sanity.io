78ccc4224a91e8642147e8f52879ee2147d709ed
Arif Mohammed P.F <arifmohammedpf77@gmail.com>
2020-12-08 13:57:24 +0530

    Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import bgImg from '../components/homeBg.jpg'
 function Home() {
     return (
-        <div>
-            <h1>home</h1>
-        </div>
+        <main>
+            <img src={bgImg} alt="background"
+                className="absolute object-cover w-full h-full"
+            />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-60 px-8">
+                <h1 className="text-6xl text-green-400 font-bold cursive leading-none lg:leading-snug home-name"
+                >hello. i'm Arif</h1>
+            </section>
+        </main>
     )
 }
 
